e2b94e8183460c436db4ca3246d44afc4062177f
kobbikobb <jakob.jonasson@gmail.com>
2012-11-28 00:26:08 +0000

    missing files namescontroller and thenames

---
+++ MvcGuessMySonsName/Controllers/NamesController.cs
@@ -0,0 +1,21 @@
+﻿using MvcGuessMySonsName.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MvcGuessMySonsName.Controllers
+{
+    //Jakob Jónasson
+    public class NamesController : ApiController
+    {
+        // GET api/names/getisname?name=
+        public bool GetIsName(string name)
+        {
+            var theName = new TheName();
+
+            return theName.IsName(name);
+        }
+    }
+}
---
+++ MvcGuessMySonsName/Models/TheName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcGuessMySonsName.Models
+{
+    //Jakob Jónasson
+    public class TheName
+    {
+        private string name = "AG9Yo9bFokfBA6y4A4JVovsh8ogQksUYdqirs/LxQtyPFwL3xV42lCG5wJApDqCcXA==";
+
+        public TheName()
+        {
+
+        }
+
+        public bool IsName(string theName)
+        {
+            return System.Web.Helpers.Crypto.VerifyHashedPassword(name, theName);
+        }
+    }
+}
