bddb45088f17d8f49a630e7ec47a569258dc7acd
kobbikobb <jakob.jonasson@gmail.com>
2012-12-22 14:39:28 +0000

    Fyrsta nafnið

--- MvcGuessMySonsName/Models/TheName.cs
+++ MvcGuessMySonsName/Models/TheName.cs
@@ -8,7 +8,8 @@ namespace MvcGuessMySonsName.Models
     //Jakob Jónasson
     public class TheName
     {
-        private string hashedName = "AG9Yo9bFokfBA6y4A4JVovsh8ogQksUYdqirs/LxQtyPFwL3xV42lCG5wJApDqCcXA==";
+        private string hashedFirstName = "AJMtM0JjY3VthyhbE/sQtRnxeRBax16qL4XpSfmwa18M9d1ZrsxOOJb9aAWbsALrjg==";
+        private string hasedFullName = "AG9Yo9bFokfBA6y4A4JVovsh8ogQksUYdqirs/LxQtyPFwL3xV42lCG5wJApDqCcXA==";
         private readonly GuessesRepository guessesRepository;
 
         public TheName()
@@ -23,10 +24,13 @@ namespace MvcGuessMySonsName.Models
 
         public bool IsName(string userName, string name)
         {
+            var isName = System.Web.Helpers.Crypto.VerifyHashedPassword(hasedFullName, name) ||
+                System.Web.Helpers.Crypto.VerifyHashedPassword(hasedFullName, name);
+
             if (!string.IsNullOrEmpty(userName))
                 guessesRepository.SaveGuess(userName, name);
 
-            return System.Web.Helpers.Crypto.VerifyHashedPassword(hashedName, name);
+            return System.Web.Helpers.Crypto.VerifyHashedPassword(hashedFirstName, name) || System.Web.Helpers.Crypto.VerifyHashedPassword(hasedFullName, name);
         }
     }
 }
